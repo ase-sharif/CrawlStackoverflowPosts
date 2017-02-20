42e16a32adc52f6720a71b7341e7e5b69a738869
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-02-20 17:08:43 +0330

    Import Question to database

--- db_connector.py
+++ db_connector.py
@@ -11,12 +11,12 @@ class DBConnector:
         self.conn = MySQLdb.connect(
             host=parser.get('DATABASE', 'HOST'),
             user=parser.get('DATABASE', 'USER'),
-            password=parser.get('DATABASE', 'password'),
+            password=parser.get('DATABASE', 'PASSWORD'),
             db=parser.get('DATABASE', 'DB'),
             use_unicode=True,
             charset="utf8")
         self.conn.set_character_set('utf8mb4')
-        self.conn.autocommit(auto_commit=True)
+        self.conn.autocommit(on=False)
         self.cursor = self.conn.cursor()
 
     def get_cursor(self):
